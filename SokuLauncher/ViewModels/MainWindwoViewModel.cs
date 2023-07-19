4ba9720bd6b13d86c490f07c33085ad144b6ac55
Miles <git@miles.tw>
2023-07-20 00:56:00 +0800

    fix: window may be out of bounds of screen

--- SokuLauncher/ViewModels/MainWindwoViewModel.cs
+++ SokuLauncher/ViewModels/MainWindwoViewModel.cs
@@ -1,9 +1,11 @@
 ﻿using SokuLauncher.Models;
+using System;
 using System.Collections.Generic;
+using System.Windows;
 
 namespace SokuLauncher.ViewModels
 {
-    public class MainWindwoViewModel: ViewModelBase
+    public class MainWindwoViewModel : ViewModelBase
     {
         private List<ModSettingGroupViewModel> _SokuModSettingGroups;
         public List<ModSettingGroupViewModel> SokuModSettingGroups
@@ -19,5 +21,25 @@ namespace SokuLauncher.ViewModels
             }
         }
         public ModSettingGroupViewModel SelectedSokuModSettingGroup { get; set; }
+
+        public double MainWrapMaxHeight
+        {
+            get
+            {
+                double workAreaHeight = SystemParameters.WorkArea.Bottom;
+                double mainWindowButtonsHeight = 48;
+                double result = Math.Floor((workAreaHeight - mainWindowButtonsHeight) / 166) * 166;
+
+                return result;
+            }
+        }
+
+        public double MainWrapMaxWidth
+        {
+            get
+            {
+                return SystemParameters.WorkArea.Right - 48;
+            }
+        }
     }
 }
