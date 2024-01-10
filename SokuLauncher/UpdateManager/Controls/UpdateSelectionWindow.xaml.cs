cdcaf60fc02cfe3d20e2e51d5a814a1f3679d5f6
Miles <git@miles.tw>
2024-01-10 20:22:14 +0800

    fix: mouse wheel scrolling to SelectorListView

--- SokuLauncher/UpdateManager/Controls/UpdateSelectionWindow.xaml.cs
+++ SokuLauncher/UpdateManager/Controls/UpdateSelectionWindow.xaml.cs
@@ -8,7 +8,9 @@ using System.ComponentModel;
 using System.Diagnostics;
 using System.IO;
 using System.Windows;
+using System.Windows.Controls;
 using System.Windows.Input;
+using System.Windows.Media;
 
 namespace SokuLauncher.Controls
 {
@@ -47,6 +49,45 @@ namespace SokuLauncher.Controls
         {
             Process.Start(new ProcessStartInfo { FileName = ((Uri)e.Parameter).AbsoluteUri, UseShellExecute = true });
             e.Handled = true;
+        }
+
+        private void SelectorListView_PreviewMouseWheel(object sender, MouseWheelEventArgs e)
+        {
+            e.Handled = true;
+
+            // 计算滚动的增量
+            double delta = e.Delta / 3.0;
+
+            // 获取外部 ScrollViewer
+            ScrollViewer scrollViewer = FindScrollViewer(SelectorListView);
+
+            // 调整滚动位置
+            if (scrollViewer != null)
+            {
+                scrollViewer.ScrollToVerticalOffset(scrollViewer.VerticalOffset - delta);
+            }
+        }
+
+        private ScrollViewer FindScrollViewer(DependencyObject depObj)
+        {
+            if (depObj == null) return null;
+
+            for (int i = 0; i < VisualTreeHelper.GetChildrenCount(depObj); i++)
+            {
+                DependencyObject child = VisualTreeHelper.GetChild(depObj, i);
+
+                if (child is ScrollViewer)
+                {
+                    return (ScrollViewer)child;
+                }
+
+                ScrollViewer result = FindScrollViewer(child);
+
+                if (result != null)
+                    return result;
+            }
+
+            return null;
         }
     }
 }
