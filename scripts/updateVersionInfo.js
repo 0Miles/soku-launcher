44ff2c72281f01ef262b6c8f1cd59260922798df
Miles <git@miles.tw>
2024-01-15 00:41:14 +0800

    ci: update versionInfo

--- scripts/updateVersionInfo.js
+++ scripts/updateVersionInfo.js
@@ -8,8 +8,17 @@ const jsonFilePath = path.join('docs', 'version.json');
 const jsonData = fs.readFileSync(jsonFilePath, 'utf8');
 const jsonContent = JSON.parse(jsonData.replace(/\n/g, '').replace(/\r/g, ''));
 
+const changelogMarkdown = fs.readFileSync('changelog.md', 'utf8');
+
 jsonContent[0].Version = version
 jsonContent[0].DownloadUrl = `https://github.com/0Miles/soku-launcher/releases/download/v${version}/SokuLauncher.zip`
+jsonContent[0].DownloadLinks = [
+    {
+        Type: 'Github',
+        Url: `https://github.com/0Miles/soku-launcher/releases/download/v${version}/SokuLauncher.zip`
+    }
+]
+jsonContent[0].Notes = changelogMarkdown;
 
 const updatedJsonData = JSON.stringify(jsonContent, null, 4);
 
