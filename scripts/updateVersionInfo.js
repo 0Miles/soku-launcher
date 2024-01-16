d45c254c654e96d32ff02e6b5d2d06ee78f8c948
Miles <git@miles.tw>
2024-01-16 14:02:38 +0800

    ci: updateVersionInfo

--- scripts/updateVersionInfo.js
+++ scripts/updateVersionInfo.js
@@ -1,44 +1,59 @@
 const fs = require('fs');
 const path = require('path');
+const https = require('https');
 
 const version = process.argv[2].replace('v', '');
+const githubToken = process.argv[3];
 
-function getReleaseNotes(owner, repo, version) {
-    const apiUrl = `https://api.github.com/repos/${owner}/${repo}/releases`;
-
+function getReleaseNotes(owner, repo, version, githubToken) {
     return new Promise((resolve, reject) => {
-        const xhr = new XMLHttpRequest();
-        xhr.open('GET', apiUrl, true);
+        const options = {
+            hostname: 'api.github.com',
+            path: `/repos/${owner}/${repo}/releases`,
+            method: 'GET',
+            headers: {
+                'User-Agent': 'node.js',
+                'Accept': 'application/vnd.github+json',
+                'Authorization': `bearer ${githubToken}`,
+                'X-GitHub-Api-Version': '2022-11-28'
+            }
+        };
 
-        xhr.onload = () => {
-            if (xhr.status >= 200 && xhr.status < 300) {
-                const releases = JSON.parse(xhr.responseText);
+        const req = https.request(options, (res) => {
+            let data = '';
+
+            res.on('data', (chunk) => {
+                data += chunk;
+            });
+
+            res.on('end', () => {
+                const releases = JSON.parse(data);
 
                 const release = releases.find(r => r.tag_name === version);
 
                 if (release) {
                     resolve(release.body);
+                } else {
+                    reject(`Release not found for version ${version}`);
                 }
-            } else {
-                reject(`Failed to fetch releases. Status code: ${xhr.status}`);
-            }
-        };
+            });
+        });
 
-        xhr.onerror = () => {
-            reject('Error making the request.');
-        };
+        req.on('error', (e) => {
+            reject(`Error: ${e.message}`);
+        });
 
-        xhr.send();
+        req.end();
     });
 }
 
-async function updateVersionInfo(version) {
+async function updateVersionInfo(version, githubToken) {
     const jsonFilePath = path.join('docs', 'version.json');
 
     const jsonData = fs.readFileSync(jsonFilePath, 'utf8');
     const jsonContent = JSON.parse(jsonData.replace(/\n/g, '').replace(/\r/g, ''));
 
-    const changelogMarkdown = await getReleaseNotes('0Miles', 'soku-launcher', version);
+    const releaseNotes = await getReleaseNotes('0Miles', 'soku-launcher', 'v' + version, githubToken);
 
     jsonContent[0].Version = version
     jsonContent[0].DownloadUrl = `https://github.com/0Miles/soku-launcher/releases/download/v${version}/SokuLauncher.zip`
@@ -48,11 +63,13 @@ async function updateVersionInfo(version) {
             Url: `https://github.com/0Miles/soku-launcher/releases/download/v${version}/SokuLauncher.zip`
         }
     ]
-    jsonContent[0].Notes = changelogMarkdown;
+    jsonContent[0].Notes = releaseNotes;
 
     const updatedJsonData = JSON.stringify(jsonContent, null, 4);
 
     fs.writeFileSync(jsonFilePath, updatedJsonData, 'utf8');
 }
 
-updateVersionInfo(version);
+(async () => {
+    await updateVersionInfo(version, githubToken);
+})();
